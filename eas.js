3ee43b7fc18ec19d67c6792f8498c828ed5811f2
Kevin Sy <kevsy@ucdavis.edu>
2023-11-26 22:41:07 -0800

    finish added eventlistener and css

--- eas.js
+++ eas.js
@@ -1,6 +1,20 @@
 const body = document.querySelector('#body');
 
-for(let i = 1; i < 16; i++) {
+const handleReset = () => {
+    let size = prompt("Enter how big you want your new grid (1-100)")
+    while(body.firstChild) {
+        body.removeChild(body.firstChild)
+    }
+    if (size > 100 || size <= 0) {
+        alert("Please Input valid size")
+        createGrid(16)
+    } else {
+        createGrid(size)
+    }
+}
+
+function createGrid(size) {
+    for(let i = 1; i < size; i++) {
     //create 16 boxes on x axis
     const xcontent = document.createElement('div');
     xcontent.classList.add(`x-box${i}`)
@@ -8,9 +22,15 @@ for(let i = 1; i < 16; i++) {
     body.appendChild(xcontent)
     //create 15 boxes on y axis
     const xbody = document.querySelector(`.x-box${i}`)
-    for(let y = 1; y < 16; y++) {
+    for(let y = 1; y < size; y++) {
         const ycontent = document.createElement('div');
         ycontent.classList.add('y-box')
+        ycontent.addEventListener('mouseout', () => {
+            ycontent.classList.add('active')
+        })
         xbody.appendChild(ycontent)
+        }
     }
 }
+
+createGrid(16)
